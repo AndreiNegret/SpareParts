9f8705cc2f0ae83682951296102d7c5cd170c5a3
Andrei Negreț <andrei.negret@yahoo.com>
2020-10-30 00:32:21 +0200

    Added authentification pages & other updates

--- SpareParts/Controllers/HomeController.cs
+++ SpareParts/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SpareParts.Models;
@@ -18,6 +19,7 @@ namespace SpareParts.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
--- SpareParts/Startup.cs
+++ SpareParts/Startup.cs
@@ -12,6 +12,8 @@ using SpareParts.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace SpareParts
 {
@@ -34,6 +36,14 @@ namespace SpareParts
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddMvc(o =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                o.Filters.Add(new AuthorizeFilter(policy));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
